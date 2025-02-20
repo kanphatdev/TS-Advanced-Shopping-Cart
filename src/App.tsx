ea07683f191a70494afc17876bfe9a253de11625
kanphatratrazer <kanphatratrazer28@gmail.com>
2025-02-20 20:38:18 +0700

    setup Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Store from "./pages/Store";
+import About from "./pages/About";
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
-export default App
+    <div className="container mx-auto px-4 py-8 ">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
